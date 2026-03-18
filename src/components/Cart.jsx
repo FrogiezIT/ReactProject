39ea7ad8ee11de128acb0dfc642d2c7dd168712a
FrogiezIT <aman@frogiez.com>
2026-03-18 10:30:03 +0530

    commit

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,22 +4,21 @@ const Cart = () => {
   const { cart, removeFromCart, total } = useCart();
 
   return (
-    <div>
-      <h2>Cart</h2>
-
+    <div className="bg-white flex justify-between m-auto w-fit py-2 px-3">
+    
       {cart.length === 0 && <p>Cart is empty</p>}
 
       {cart.map((item, index) => (
         <div key={index}>
-          {item.name} - ${item.price}
+          {item.name}  ${item.price} 
 
-          <button onClick={() => removeFromCart(index)}>
+          <button className="ml-2" onClick={() => removeFromCart(index)}>
             Remove
           </button>
         </div>
       ))}
 
-      <h3>Total: ${total}</h3>
+      <h3 className="ml-2">Total: ${total}</h3>
     </div>
   );
 };
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -10,14 +10,13 @@ const ProductList = () => {
   const { addToCart } = useCart();
 
   return (
-    <div>
-      <h2>Products</h2>
-
+    <div className="flex flex-row gap-5 m-auto justify-center my-5">
       {products.map((product) => (
-        <div key={product.id}>
-          <p>{product.name} - ${product.price}</p>
+        <div key={product.id} className="bg-blue-950 p-5 rounded-2xl w-1/5">
+          <p className="text-white">{product.name}</p>
+          <p className="text-white my-3">${product.price}</p>
 
-          <button onClick={() => addToCart(product)}>
+          <button className="bg-white text-black px-8 py-2 rounded-xs" onClick={() => addToCart(product)}>
             Add to Cart
           </button>
 
